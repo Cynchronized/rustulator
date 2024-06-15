55a6136391992f82fa3bc1a26bb2c4fb5056fbd3
Cynchronized <austin.thieu123@gmail.com>
2024-06-15 15:07:02 -0600

    Created cpu with LDA, TAX, and INX opcode

---
+++ target/debug/deps/nes_emulator-10de5f8a992af67f.d
@@ -0,0 +1,11 @@
+/home/cync/rust-projects/nes-emulator/target/debug/deps/libnes_emulator-10de5f8a992af67f.rmeta: src/main.rs src/cpu/mod.rs src/cpu/cpu.rs Cargo.toml
+
+/home/cync/rust-projects/nes-emulator/target/debug/deps/nes_emulator-10de5f8a992af67f.d: src/main.rs src/cpu/mod.rs src/cpu/cpu.rs Cargo.toml
+
+src/main.rs:
+src/cpu/mod.rs:
+src/cpu/cpu.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
